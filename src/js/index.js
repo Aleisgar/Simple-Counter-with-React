8359c503d7624a645999b75dedaa65ce57c38a4c
Aleisgar <aleisgar@gmail.com>
2022-06-04 16:41:49 +0000

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,60 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+const SecondCounter = (props) => {
+	console.log(props);
+	return (
+		<h1 className="Title m-2 text-center">
+			My First Counter
+			<div className="BigCounter">
+				<div className="digit m-2">{props.semana}</div>
+				<div className="digit m-2">{props.dia}</div>
+				<div className="digit m-2">{props.horas}</div>
+				<div className="digit m-2">{props.minutos}</div>
+				<div className="digit m-2">{props.segundos}</div>
+			</div>
+		</h1>
+	);
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+// const Seconds = () => {
+//
+//return counter;
+// };
+let counter = 0;
+let counter2 = 0;
+let counter3 = 0;
+let counter4 = 0;
+let counter5 = 0;
+
+setInterval(function () {
+	counter++;
+	if (counter === 9) {
+		counter = 0;
+		counter2++;
+		if (counter2 === 6) {
+			counter2 = 0;
+			counter3++;
+			if (counter3 === 24) {
+				counter3 = 0;
+				counter4++;
+				if (counter4 === 7) {
+					counter4 = 0;
+					counter5++;
+				}
+			}
+		}
+	}
+
+	//console.log(counter);
+	ReactDOM.render(
+		<SecondCounter
+			segundos={counter}
+			minutos={counter2}
+			horas={counter3}
+			dia={counter4}
+			semana={counter5}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
